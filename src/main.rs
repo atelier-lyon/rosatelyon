ba5a9b59f37dfd066ca310911207362ad86a9a30
vianney.marticou <la.regle.du.jeu.ytb@gmail.com>
2023-11-29 23:08:32 +0100

    [MAIN] Setup the scheduleRunner for a loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 pub mod cli;
 pub mod mqtt;
 
-use bevy::prelude::*;
+use bevy::{app::ScheduleRunnerPlugin, prelude::*};
 use clap::Parser;
 use cli::{ressources::Args, system::print_arg};
 use mqtt::connect_every_broker;
+use std::time::Duration;
+
+const FREQUENCY: f64 = 200.0;
+
+fn heartbeat() {
+    println!("Heartbeat !");
+}
 
 fn hello_world() {
     println!("Good to see you !");
@@ -17,5 +24,9 @@ fn main() {
         .add_systems(Startup, hello_world)
         .add_systems(Startup, print_arg)
         .add_systems(Startup, connect_every_broker)
+        .add_systems(Update, heartbeat)
+        .add_plugins(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
+            1.0 / FREQUENCY,
+        )))
         .run();
 }

0b80c2ebf7985c048e05dcd17849d949723fe7c1
vianney.marticou <la.regle.du.jeu.ytb@gmail.com>
2023-11-29 22:30:29 +0100

    [MQTT] Implement the system connect_every_broker in bevy

--- src/mqtt/mod.rs
+++ src/mqtt/mod.rs
@@ -1,19 +1,34 @@
-use rumqttc::{Client, MqttOptions};
+use rumqttc::{Client, Connection, MqttOptions};
 use std::time::Duration;
 
 use crate::cli::ressources::Args;
 use bevy::prelude::*;
 
-pub fn connect_every_broker(args: Res<Args>) {
+#[derive(Component)]
+struct ClientConnection(Client, Connection);
+
+// NOTE: Implement sync for the derive Component
+// Could be useful to find a better solution
+unsafe impl Sync for ClientConnection {}
+
+/// Connect every the program to every broker
+/// Add a Component, ClientConnection for everty new connection
+pub fn connect_every_broker(mut commands: Commands, args: Res<Args>) {
     for broker in args.mqtt_server.iter() {
-        connect_client(broker);
+        commands.spawn(connect_client(broker));
     }
 }
 
-fn connect_client(broker: &String) {
+/// Connect a client with a mqtt broker
+/// Format of the broker <server:port>
+///
+/// Return: ClientConnection
+fn connect_client(broker: &String) -> ClientConnection {
+    // TODO: Extract the port from the broker string <server:port>
     let mut mqttoptions = MqttOptions::new("Robot", broker, 1883);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
-    let (mut _client, mut _connection) = Client::new(mqttoptions, 10);
+    let (client, connection) = Client::new(mqttoptions, 10);
     println!("Connected to {}", broker);
+    ClientConnection(client, connection)
 }
